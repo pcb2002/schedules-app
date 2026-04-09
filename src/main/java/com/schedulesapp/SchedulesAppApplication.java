5b57e438ebf74368f67d54dbcc15e2f18720b5b8
pcb2002 <machat350@gmail.com>
2026-04-09 16:06:16 +0900

    [Build]: setting

--- src/main/java/com/schedulesapp/SchedulesAppApplication.java
+++ src/main/java/com/schedulesapp/SchedulesAppApplication.java
@@ -1,4 +1,4 @@
-package com.example.schedulesapp;
+package com.schedulesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/schedulesapp/SchedulesAppApplicationTests.java
+++ src/test/java/com/schedulesapp/SchedulesAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.schedulesapp;
+package com.schedulesapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
